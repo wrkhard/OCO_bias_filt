f26c13952ec8625aec648e381502d9ffbd8eb609
Steffen Mauceri <steffen.mauceri@jpl.nasa.gov>
2025-10-15 08:15:12 -0700

    made compatible with multi dimensional data

--- export/Validate_Lite_file_export.py
+++ export/Validate_Lite_file_export.py
@@ -9,33 +9,38 @@ from tqdm import tqdm
 import os
 from util import plot_map, dist
 
-def get_all_headers(f):
-    headers = list(f.variables.keys())
-
-    # get groups
+def get_all_headers_with_dims(f):
+    headers = []
+    dims = []
+    # Get variables in the root group
+    for var_name in f.variables.keys():
+        var = f.variables[var_name]
+        headers.append(var_name)
+        dims.append(var.ndim)
+    # Get variables in subgroups
     groups = list(f.groups.keys())
-
     for g in groups:
-        vars = list(f[g].variables.keys())
-        for v in vars:
-            headers.append(g+ '/' + v )
-
-    return headers
+        for var_name in f[g].variables.keys():
+            full_var_name = g + '/' + var_name
+            var = f[g].variables[var_name]
+            headers.append(full_var_name)
+            dims.append(var.ndim)
+    return headers, dims
 
 
 year = 2015
 frac = 0.5 # fraction of data to load
 name_all = 'LiteFileExport_' + str(year) + '_'
 save_fig = True
-Lite_file_path = '/Volumes/OCO/LiteFiles/B11_gamma/'
-save_path = '/Volumes/OCO/LiteFiles/B11_gamma_val/'
+Lite_file_path = '/Volumes/OCO/LiteFiles/export/B11.2_ML/'
+save_path = '/Volumes/OCO/LiteFiles/B11.2v2ML_val/'
 
 # check that we exported each file from B111 to B112
 import os
 
 # Paths to the two directories
-B111_dir = '/Volumes/OCO/LiteFiles/B111/'
-B112_dir = Lite_file_path
+OCO2_original_dir = '/Volumes/OCO/LiteFiles/B11.2_OCO2/'
+OCO2_ML_dir = Lite_file_path
 
 # Function to extract file identifiers from file names
 def extract_identifier(filename):
@@ -48,15 +53,15 @@ def extract_identifier(filename):
     return filename
 
 # List of file identifiers in each directory
-files_in_dir1 = {extract_identifier(f) for f in os.listdir(B111_dir) if os.path.isfile(os.path.join(B111_dir, f))}
-files_in_dir2 = {extract_identifier(f) for f in os.listdir(B112_dir) if os.path.isfile(os.path.join(B112_dir, f))}
+files_in_dir1 = {extract_identifier(f) for f in os.listdir(OCO2_original_dir) if os.path.isfile(os.path.join(OCO2_original_dir, f))}
+files_in_dir2 = {extract_identifier(f) for f in os.listdir(OCO2_ML_dir) if os.path.isfile(os.path.join(OCO2_ML_dir, f))}
 
 # Identifiers unique to each directory
 unique_to_dir1 = files_in_dir1 - files_in_dir2
 unique_to_dir2 = files_in_dir2 - files_in_dir1
 
-print("Files unique to B111 directory:", unique_to_dir1)
-print("Files unique to B11gamma directory:", unique_to_dir2)
+print("Files unique to B11.2_OCO2 directory:", unique_to_dir1)
+print("Files unique to B11.2_ML directory:", unique_to_dir2)
 
 
 
@@ -73,46 +78,90 @@ if save_fig:
 fill_value = 999999
 counts=0
 print(year)
-data_all = []
 # get LiteFile data
 Lite_files = glob.glob(Lite_file_path + 'oco2_LtCO2_' + str(year-2000) + '*.nc4')
 
 # get Lite vars
 l_ds = nc.Dataset(Lite_files[0])
-l_vars = get_all_headers(l_ds)
+l_vars, l_dims = get_all_headers_with_dims(l_ds)
 # remove vars we dont need
-l_vars = [e for e in l_vars if e not in ('bands', 'footprints','levels', 'vertices','Retrieval/iterations','file_index','vertex_latitude','vertex_longitude',
-                                         'date','source_files','pressure_levels', 'co2_profile_apriori', 'xco2_averaging_kernel','Preprocessors/co2_ratio_offset_per_footprint',
-                                         'Preprocessors/h2o_ratio_offset_per_footprint','Retrieval/SigmaB',
-                                         'pressure_weight', 'xco2_qf_simple_bitflag', 'xco2_qf_bitflag', 'Sounding/l1b_type', 'Sounding/orbit')]
+vars_to_remove = [
+    'bands', 'footprints', 'levels', 'vertices', 'Retrieval/iterations', 'file_index', 'vertex_latitude',
+    'vertex_longitude', 'date', 'source_files', 'pressure_levels', 'co2_profile_apriori', 'xco2_averaging_kernel',
+    'Preprocessors/co2_ratio_offset_per_footprint', 'Preprocessors/h2o_ratio_offset_per_footprint',
+    'Retrieval/SigmaB', 'pressure_weight', 'xco2_qf_simple_bitflag', 'xco2_qf_bitflag',
+    'Sounding/l1b_type', 'Sounding/orbit', 'frames',
+]
+l_vars = [e for e in l_vars if e not in vars_to_remove]
+# remove any vars that start with 'L1b'
+l_vars = [e for e in l_vars if not e.startswith('L1b')]
+
+# Separate variables by dimension
+l_vars_1d = []
+l_vars_2d = []
+l_vars_nd = []
+for i, v in enumerate(l_vars):
+    var = l_ds[v]
+    ndim = var.ndim
+    if ndim == 1:
+        l_vars_1d.append(v)
+    elif ndim == 2:
+        l_vars_2d.append(v)
+    else:
+        l_vars_nd.append(v)
+
+# Initialize data_dict keys
+data_dict = {}
+for v in l_vars_1d + l_vars_2d + l_vars_nd:
+    data_dict[v] = []
 
 # read in data
 for j in tqdm(range(len(Lite_files))):
     l = Lite_files[j]
     l_ds = nc.Dataset(l)
-    l_ids = l_ds['sounding_id'][:]
-    # initialize array
-    data = np.ones((len(l_ids ), len(l_vars))) * fill_value
 
-    # load cloud data
-    i = -1
-    for v in l_vars:
-        i += 1
-        data[:, i] = l_ds[v][:]
+    # Read 1D variables
+    for v in l_vars_1d:
+        val = l_ds[v][:]
+        val = np.where(val == fill_value, np.nan, val)
+        data_dict[v].append(val)
 
-    # change missing values to nan
-    data[data == fill_value] = np.nan
+    # Read 2D variables
+    for v in l_vars_2d:
+        val = l_ds[v][:]  # shape: (num_soundings, dim2)
+        val = np.where(val == fill_value, np.nan, val)
+        data_dict[v].append(val)
 
-    # downsample data to save RAM
-    interval = int(1 / frac)
-    data = data[::interval, :]
+    # Handle variables with ndim > 2
+    for v in l_vars_nd:
+        val = l_ds[v][:]  # shape: (num_soundings, dim2, dim3, ...)
+        val = np.where(val == fill_value, np.nan, val)
+        data_dict[v].append(val)
 
-    data_all.append(data)
-    counts+=1
+    counts += 1
 
+# Concatenate data from all files
+for v in data_dict.keys():
+    data_dict[v] = np.concatenate(data_dict[v], axis=0)
 
-data = np.concatenate(data_all)
-data = pd.DataFrame(data, columns=l_vars)
+# Create DataFrame
+data = pd.DataFrame()
+
+# Add 1D variables to DataFrame
+for v in l_vars_1d:
+    data[v] = data_dict[v]
+
+# Add 2D variables to DataFrame (each entry is an array)
+for v in l_vars_2d:
+    data[v] = list(data_dict[v])
+
+# Handle variables with ndim > 2 if needed (each entry is an array)
+for v in l_vars_nd:
+    data[v] = list(data_dict[v])
+
+# downsample data to save RAM
+interval = int(1 / frac)
+data = data.iloc[::interval]
 data['sounding_id'] = data['sounding_id'].astype(int)
 
 # clean up features
@@ -131,7 +180,7 @@ lat = data['latitude'].to_numpy()
 lon = data['longitude'].to_numpy()
 SA = np.zeros_like(lat)*np.nan
 SA_bias_b112 = np.zeros_like(lat)*np.nan
-XCO2b112 = data['xco2_gamma'].to_numpy()
+XCO2b112 = data['xco2_ML'].to_numpy()
 SA_bias = np.zeros_like(lat) * np.nan
 XCO2 = data['xco2'].to_numpy()
 
@@ -167,10 +216,10 @@ while i+j < len(data):
 with open(save_path + 'SA_bias.txt', 'w') as f:
     for qf in [0, 1, 2]:
         # calculate mean and std of SA bias
-        xco2_SA_bias_mean = np.nanmean(SA_bias[data['xco2_quality_flag_gamma']==qf])
-        xco2b112_SA_bias_mean = np.nanmean(SA_bias_b112[data['xco2_quality_flag_gamma']==qf])
-        xco2_SA_bias_std = np.nanstd(SA_bias[data['xco2_quality_flag_gamma']==qf])
-        xco2b112_SA_bias_std = np.nanstd(SA_bias_b112[data['xco2_quality_flag_gamma']==qf])
+        xco2_SA_bias_mean = np.nanmean(SA_bias[data['xco2_quality_flag_ML']==qf])
+        xco2b112_SA_bias_mean = np.nanmean(SA_bias_b112[data['xco2_quality_flag_ML']==qf])
+        xco2_SA_bias_std = np.nanstd(SA_bias[data['xco2_quality_flag_ML']==qf])
+        xco2b112_SA_bias_std = np.nanstd(SA_bias_b112[data['xco2_quality_flag_ML']==qf])
 
         # write to txt file
         f.write('Quality Flag: ' + str(qf) + '\n')
@@ -181,8 +230,8 @@ with open(save_path + 'SA_bias.txt', 'w') as f:
 
 
 # visualize data ***************************************************************
-data.loc[:,'ML-B11'] = data.loc[:,'xco2_gamma'] - data.loc[:,'xco2']
-data.loc[:,'ML-Raw'] = data.loc[:,'xco2_gamma'] - data.loc[:,'xco2_raw']
+data.loc[:,'ML-B11'] = data.loc[:,'xco2_ML'] - data.loc[:,'xco2']
+data.loc[:,'ML-Raw'] = data.loc[:,'xco2_ML'] - data.loc[:,'xco2_raw']
 data.loc[:,'B11-Raw'] = data.loc[:,'xco2'] - data.loc[:,'xco2_raw']
 data_all = data.copy()
 for qf in [0, 1, 2]:
